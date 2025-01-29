681210b714024cec9eb5a96d968d15a9fcf685c5
Jacob Cotham <JACotham@student.fullsail.edu>
2025-01-29 14:47:31 -0500

    UnitTest Project created

---
+++ HelloWorldTesting/HelloWorldTesting.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HelloWorldTesting
+{
+	TEST_CLASS(HelloWorldTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
